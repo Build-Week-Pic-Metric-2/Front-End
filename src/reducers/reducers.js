11001605cd4f547e3d998c72c022d4158b2dff3c
JChartier001 <JChartier001@gmail.com>
2019-11-18 12:11:37 -0600

    imported actions to reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import {FETCH_USERPICS_START, FETCH_USERPICS_SUCCESS, FETCH_USERPICS_FAILURE, POST_USERPICS_START, POST_USERPICS_SUCCESS, POST_USERPICS_FAILURE, DELETE_USERPICS_START, DELETE_USERPICS_SUCCESS, DELETE_USERPICS_FAILURE, EDIT_USERPICS_START, EDIT_USERPICS_SUCCESS, EDIT_USERPICS_FAILURE} from "../actions/actions"
 
-
-export const initialState = () =>{
+export const initialState = {
+    pics: [],
+    isLoading: false,
+    error: null
 
 }
 export const reducer=(state=initialState, actions)=> {
     switch(actions.type){
+
         
     }
 }
