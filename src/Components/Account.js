4db1bfa8ca8910ea7b9ce1b1aa0c795bc5cfcdd6
JChartier001 <JChartier001@gmail.com>
2019-11-19 19:17:40 -0600

    correct info is being sent, but not posting.

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,34 +1,41 @@
 import React, {useState} from 'react'
 import {useSelector, useDispatch} from "react-redux";
-
-
+import ImageUploader from 'react-images-upload';
 
 import PhotoList from "./PhotoList"
 import { postPics } from '../actions/actions';
 
 
-
 const Account = () =>{
+
     const state = useSelector(state => state);
-    const [newPhoto, setNewPhoto]=useState({
-      title:"",
-      description: ""
+    const [newPhoto, setNewPhoto]=useState({      
+      title: '',
+      description: '',
     });
+    const [images, setImages] = useState([])
     const dispatch = useDispatch();
     console.log("Account", state)
     const username = sessionStorage.getItem("username")
 
     const handleChange = (e) => {
       setNewPhoto({...newPhoto,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,     
     })
     }
 
+     const onDrop = (pic) => {
+      setImages(
+          state.pics.concat(pic),
+      );
+    }
+
    const handleUpload=()=>{
-     dispatch(postPics(state.pics))    
+     dispatch(postPics(newPhoto, images))    
    }
 
    console.log(newPhoto)
+   console.log(images)
     
     return (
         <div className="account-container">
@@ -36,24 +43,23 @@ const Account = () =>{
              <p>Upload a New Image:</p>
               <label>Title: <input type="text" placeholder="Title" name="title" onChange={handleChange}/></label>
               <label>Description: <input type="text" placeholder="Description of Photo" name="description" onChange={handleChange}/></label>
-              <label>Image: <input type='file' id='single' onClick={handleUpload}/></label>
-            
+              {/* <label>Image: <input type='file' id='single'/></label>
+              */}
+             <ImageUploader
+          withIcon={true}
+          buttonText='Choose images'
+          onChange={onDrop}
+          imgExtension={['.jpg', '.gif', '.png', '.gif']}
+          maxFileSize={5242880}
+      />
+      <button type="submit" onClick={handleUpload} name="image">Upload Image</button> 
 
-            {state.error && <p>Error: {state.error}</p>}
+          
+            {/* {state.error && <p>Error: {state.error}</p>}
         {state.isLoading ? 
         (
           <div>...Loading</div>
-        ) : (
-          <div>
-         <PhotoList />
-          </div>
-        )}
-
-            {state.error && <p>Error: {state.error}</p>}
-        {state.isLoading ? 
-        (
-          <div>...Loading</div>
-        ) : (
+        ) : ( */}
           <div>
          <PhotoList />
           </div>
