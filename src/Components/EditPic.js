2d2d34ca6b32862f35e9c4f276e877c33b2d0794
Ashley Dunham <xenocide21@gmail.com>
2019-11-21 11:55:18 -0700

    minor edits to input type and placeholder

--- src/Components/EditPic.js
+++ src/Components/EditPic.js
@@ -57,8 +57,8 @@ const EditPic = (props) => {
             <form onSubmit={handleSubmit}>
                 {pic && pic.length > 0 ? 
                 pic.tag.map(pic=>{
-                return <label> Title: <Input type="text" name="title" placehold="Movie Title" value={pic.title} onChange={handleChange}/></label>}):null}
-                <label>Description: <Input type="text" name="director" placehold="Movie Director" value={pic.description} onChange={handleChange}/></label>
+                return <label> Title: <Input type="text" name="title" placeholder="Title" value={pic.title} onChange={handleChange}/></label>}):null}
+                <label>Description: <Input type="text" name="description" placeholder="Description" value={pic.description} onChange={handleChange}/></label>
                 <img src={image} width="400" alt={state.pics.description} /> 
                 <button className="update-save" type="submit">Save</button>
             </form>
